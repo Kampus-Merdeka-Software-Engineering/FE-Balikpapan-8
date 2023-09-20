bad10de347edfb395f40e99e05d8549f6f696b16
zyxkoo15 <ykkhmrdn@gmail.com>
2023-09-21 04:17:40 +0700

    add responsive page products and about

--- components/footer.js
+++ components/footer.js
@@ -6,143 +6,217 @@ class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <style>
-    /* Footer */
-    footer {
-      background-color: var(--accentColorDark);
-      color: var(--primaryColor);
-      padding: 130px 74px;
-      display: flex;
-      justify-content: space-between;
-    }
+    
+footer {
+  background-color: var(--accentColorDark);
+  color: var(--primaryColor);
+  padding: 130px 74px;
+  display: flex;
+  justify-content: space-between;
+}
 
-    .footer-content {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      width: 100%;
-    }
+.footer-content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+}
 
-    /* Styling for the Footer Brand (footer-brand) */
-    .footer-brand h1 {
-      font-size: 40px;
-      font-weight: 900;
-      letter-spacing: 1.2px;
-      margin-bottom: 10px;
-    }
+.footer-brand h1 {
+  font-size: 40px;
+  font-weight: 900;
+  letter-spacing: 1.2px;
+  margin-bottom: 10px;
+}
 
-    .footer-brand p {
-      color: var(--accentColorGrey);
-      font-size: 24px;
-      font-weight: 400;
-      line-height: 40px;
-      letter-spacing: 0.48px;
-    }
+.footer-brand p {
+  color: var(--accentColorGrey);
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 40px;
+  letter-spacing: 0.48px;
+}
 
-    /* Styling for the Footer Social Icons (footer-social) */
-    .footer-social {
-      display: flex;
-      margin-top: 20px;
-    }
+.footer-social {
+  display: flex;
+  margin-top: 20px;
+}
 
-    .footer-social a {
-      display: inline-block;
-      margin-right: 20px;
-      text-decoration: none;
-      color: var(--accentColorDark);
-      background-color: var(--secondaryColor);
-      padding: 12px 16px;
-      border: 1px solid var(--secondaryColor);
-      border-radius: 16px;
-      -webkit-border-radius: 16px;
-      -moz-border-radius: 16px;
-      -ms-border-radius: 16px;
-      -o-border-radius: 16px;
-      transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out,
-        transform 0.2s ease-in-out;
-      transform: scale(1);
-    }
+.footer-social a {
+  display: inline-block;
+  margin-right: 20px;
+  text-decoration: none;
+  color: var(--accentColorDark);
+  background-color: var(--secondaryColor);
+  padding: 12px 16px;
+  border: 1px solid var(--secondaryColor);
+  border-radius: 16px;
+  -webkit-border-radius: 16px;
+  -moz-border-radius: 16px;
+  -ms-border-radius: 16px;
+  -o-border-radius: 16px;
+  transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out,
+    transform 0.2s ease-in-out;
+  transform: scale(1);
+}
 
-    .footer-social a:hover {
-      background-color: transparent;
-      color: var(--secondaryColor);
-      transform: scale(1.1);
-    }
+.footer-social a:hover {
+  background-color: transparent;
+  color: var(--secondaryColor);
+  transform: scale(1.1);
+}
 
-    .footer-social a:active {
-      transform: scale(0.9);
-      /* Shrink the icon when clicked */
-    }
+.footer-social a:active {
+  transform: scale(0.9);
+}
 
-    /* Styling for the Footer Menu (footer-menu) */
-    .footer-menu {
-      display: flex;
-      width: 100%;
-     justify-content: space-evenly;
-    }
+.footer-menu {
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+}
 
-    .category{
-      margin-right: 20px;
-    }
+.category {
+  margin-right: 20px;
+}
 
-    .category-catalog-2 {
-      margin-top: 38px;
-    }
+.category-catalog-2 {
+  margin-top: 38px;
+}
 
-  
+.list-menu a {
+  position: relative;
+}
 
-    /* Modify the width of the category divs */
-   
-    .list-menu a {
-      position: relative;
-    }
+.list-menu a::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background-color: var(--accentColorGrey);
+  bottom: 0;
+  left: 0;
+  transform: scaleX(0);
+  transform-origin: bottom right;
+  transition: transform 0.2s ease-in-out;
+}
 
-    .list-menu a::before {
-      content: "";
-      position: absolute;
-      width: 100%;
-      height: 2px;
-      background-color: var(--accentColorGrey);
-      /* Warna garis bawah */
-      bottom: 0;
-      left: 0;
-      transform: scaleX(0);
-      /* Awalnya, skala garis adalah 0 */
-      transform-origin: bottom right;
-      transition: transform 0.2s ease-in-out;
-      /* Animasi transform dengan durasi 0.2 detik */
-    }
+.list-menu a:hover::before {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
 
-    .list-menu a:hover::before {
-      transform: scaleX(1);
-      /* Saat dihover, skala garis menjadi 1 (penuh) */
-      transform-origin: bottom left;
-    }
+.menu-header {
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 40px;
+  letter-spacing: 0.48px;
+}
 
-    .menu-header {
-      font-size: 24px;
-      font-weight: 600;
-      line-height: 40px;
-      letter-spacing: 0.48px;
-    }
+.list-menu {
+  font-weight: 400;
+  line-height: 40px;
+  letter-spacing: 0.48px;
+}
 
-    .list-menu {
-      font-weight: 400;
-      line-height: 40px;
-      letter-spacing: 0.48px;
-    }
+.footer-menu a {
+  color: var(--accentColorGrey);
+  text-decoration: none;
+  transition: color 0.2s ease-in-out;
+}
 
-    .footer-menu a {
-      color: var(--accentColorGrey);
-      text-decoration: none;
-      transition: color 0.2s ease-in-out;
-      /* Animasi perubahan warna teks saat dihover */
-    }
+.footer-menu a:hover {
+  color: var(--primaryColor);
+}
 
-    .footer-menu a:hover {
-      color: var(--primaryColor);
-      /* Warna teks saat dihover */
-    }
+/* Media Queries for Responsive Design */
+@media (max-width: 720px) {
+footer {
+    padding: 80px 24px;
+    flex-direction: column;
+    text-align: center;
+}
 
+.footer-content {
+    align-items: center;
+}
+
+.footer-brand h1 {
+    font-size: 32px; 
+}
+
+.footer-brand p {
+    font-size: 20px; 
+}
+
+.footer-social {
+    margin-top: 16px; 
+}
+
+.footer-social a {
+    margin-right: 12px; 
+    padding: 10px 14px;
+    font-size: 18px; 
+}
+
+.footer-menu {
+  text-align: left;
+    margin-top: 24px; 
+}
+
+.category {
+    margin-right: 0;
+    margin-bottom: 20px;
+}
+
+.category-catalog-2 {
+    margin-top: 40px; 
+}
+
+.menu-header {
+    font-size: 20px; 
+}
+
+.list-menu {
+    font-size: 18px; 
+}
+}
+
+@media (min-width: 721px) and (max-width: 1080px) {
+footer {
+    padding: 100px 40px; 
+}
+
+.footer-brand h1 {
+    font-size: 36px; 
+}
+
+.footer-brand p {
+    font-size: 22px; 
+}
+
+.footer-social a {
+    padding: 12px 16px; 
+    font-size: 20px; 
+}
+
+.category {
+    margin-right: 16px; 
+}
+
+.category-catalog-2 {
+  margin-left: 30px;
+    margin-top: 40px; 
+}
+
+.menu-header {
+    font-size: 22px; 
+}
+
+.list-menu {
+    font-size: 20px; 
+}
+}
     /* End Footer */
   </style>
   <!-- Footer -->
