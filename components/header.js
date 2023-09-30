9e47955fd296bda779baf81b0f89ec8edebf1dd5
zyxkoo15 <ykkhmrdn@gmail.com>
2023-09-30 16:26:02 +0700

    rename home -> index

--- components/header.js
+++ components/header.js
@@ -342,7 +342,7 @@ class Header extends HTMLElement {
 
       <div class="navbar-links" data-aos="zoom-in" data-aos-duration:"1000">
           <ul>
-              <li><a href="../home.html" id="homeLink">Home</a></li>
+              <li><a href="../index.html" id="homeLink">Home</a></li>
               <li><a href="../views/products.html" id="productsLink">Products</a></li>
               <li><a href="../views/about.html" id="aboutLink">About</a></li>
           </ul>
